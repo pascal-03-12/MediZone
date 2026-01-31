800ddb147d5c22dc7636f198e2ab08b0b6ca7b87
ON24-MK <michelle_kleindienst@yahoo.com>
2026-01-31 17:17:25 +0100

    feat: Abstände Pfeil-Icon angepasst

--- src/components/ManualIntakeDialog.vue
+++ src/components/ManualIntakeDialog.vue
@@ -326,3 +326,14 @@ const saveIntake = async () => {
     </div>
   </div>
 </template>
+
+<style scoped>
+select {
+  appearance: none;
+  background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%236b7280' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e");
+  background-repeat: no-repeat;
+  background-position: right 0.7rem center;
+  background-size: 1.25em;
+  padding-right: 2rem;
+}
+</style>
